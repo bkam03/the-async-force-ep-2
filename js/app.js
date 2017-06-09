a170815c3fb8ecf2c9be2f5c94a7fc7b0b13f142
BK <bkam03@yahoo.com>
2017-06-08 21:33:55 -1000

    people formatting works, puts data on page.  clears contentContainer when you request new info

--- js/app.js
+++ js/app.js
@@ -1,43 +1,79 @@
-/*
-Construct your XHR requests in the app.js file by scraping the value from the input field.
-*/
+function makeMeAnElement ( elementType, className, content ){
+  var newElement = document.createElement( elementType );
+  newElement.className = className;
+  newElement.innerHTML = content;
 
-var requestResourceButton = document.getElementById( 'requestResourceButton' );
-var resourceType = document.querySelector( '#resourceType' );
-var resourceId = document.querySelector( '#resourceId' );
+  var contentContainer = document.querySelector( '#contentContainer' );
+  contentContainer.appendChild( newElement );
+}
 
-requestResourceButton.addEventListener( 'click', function (){
-  var urlShard = resourceType.value + '/' + resourceId.value + '/';
-  var newXhrRequest = new XMLHttpRequest();
-  newXhrRequest.addEventListener( 'load', function(){
-    var targetContentContainer = document.querySelector( '#contentContainer' );
-    targetContentContainer.innerHTML = JSON.parse( this.responseText );
-    console.log( JSON.parse( this.responseText ) );
-  } );
-  newXhrRequest.open( 'GET', 'http://swapi.co/api/' + urlShard );
-  newXhrRequest.send();
-} );
-
-
-/*
-when you push the button
-it takes type and id, combining for url shard.
-creates and executes a xhr request for that thing.
-puts the data into the #contentContainer.
-
-format after.
-*/
-
-
-
-/*
-A request to the SWAPI api should be initiated when the button is clicked. Information that comes back from the API should be displayed in the body of the html. Additional html elements should be created to properly parse, display and format the data in a presentable way.
-
-If a person is requested, display the following information:
+/*If a person is requested, display the following information:
 
 Name, in an <h2> tag
 Gender, in an <p> tag
-Species (only get the first species if there are multiple) (only display the name), in an <p> tag
+Species (only get the first species if there are multiple) (only display the name), in an <p> tag*/
+
+function formattingForPeople ( contentObject ){
+  makeMeAnElement( 'h2', 'name', contentObject.name );
+  makeMeAnElement( 'p', 'gender', contentObject.gender );
+
+  var requestSpecies = new XMLHttpRequest();
+  requestSpecies.addEventListener( 'load', function (){
+    var response = JSON.parse( this.responseText );
+    console.log( response );
+    makeMeAnElement('p', 'species', response.name );
+  } );
+  requestSpecies.open( 'GET', contentObject.species );
+  requestSpecies.send();
+
+
+}
+
+function clearContents (){
+  var contentContainer = document.querySelector('#contentContainer' );
+  contentContainer.innerHTML = "";
+}
+
+( function (){
+
+  var requestResourceButton = document.getElementById( 'requestResourceButton' );
+  var resourceType = document.querySelector( '#resourceType' );
+  var resourceId = document.querySelector( '#resourceId' );
+
+  requestResourceButton.addEventListener( 'click', function (){
+    clearContents();
+
+    var urlShard = resourceType.value + '/' + resourceId.value + '/';
+    var newXhrRequest = new XMLHttpRequest();
+    newXhrRequest.addEventListener( 'load', function(){
+
+      var targetContentContainer = document.querySelector( '#contentContainer' );
+      var formattingType = resourceType.value;
+      var content = JSON.parse( this.responseText );
+      console.log( 'formatting type ', formattingType );
+      switch( formattingType ) {
+        case 'people':
+          console.log( 'people format' );
+          formattingForPeople( content );
+          break;
+        case 'planets':
+          console.log( 'planet format' );
+          break;
+        case 'starships':
+          console.log( 'starship format' );
+          break;
+        default:
+          break;
+      }
+
+    } );
+    newXhrRequest.open( 'GET', 'http://swapi.co/api/' + urlShard );
+    newXhrRequest.send();
+  } );
+
+} )();
+/*
+A request to the SWAPI api should be initiated when the button is clicked. Information that comes back from the API should be displayed in the body of the html. Additional html elements should be created to properly parse, display and format the data in a presentable way.
 
 If a planet is requested, display the following information:
 Name, in an <h2> tag
@@ -50,5 +86,7 @@ Name, in an <h2> tag
 Manufacturer, in an <p> tag
 Starship Class, in an <p> tag
 a list of all Film names that this starship appeared in <li> tags wrapped in <ul>
+
+
 Display any xhr errors in the dom, so the user can see. For example, if a user inputs 99999 as an id, or potato. Display the error on the page.
 */
